c8fe24936a582de9fa7c2e269918d487a29abfe3
GeffersonDominguez <geffersondominguez93@gmail.com>
2021-07-07 20:57:53 -0500

    Repaso de variables _tipo de datos 07-07-2021

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+nombreCompletoDelEstudiante = `Gefferson Dominguez Galeano`;
+miEdad= 28;
+soyUnEstudiante =true;
+miProfesion = `Ingeniero Industrial`;
+experiencia = `7 años`;
+direccion = `Carrera 10an # 24 - 86 T4 apto 5019`;
+estatura= 1.70;
+pasatiempo= `Jugar Futbol`
+
+
+console.log (miEdad, typeof(miEdad));
+console.log (nombreCompletoDelEstudiante, typeof(nombreCompletoDelEstudiante));
+console.log (soyUnEstudiante, typeof(soyUnEstudiante));
+console.log(miProfesion, typeof(miProfesion));
+console.log(direccion, typeof (direccion));
+document.write (estatura)
+console.log (pasatiempo)
+alert (pasatiempo)
+
+
+
